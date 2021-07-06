c38ca90baad0855f3640356269b103747a178ef8
AnthonyMSalvati <ams6995@rit.edu>
2021-07-06 18:13:56 -0400

    Able to query database tables

--- src/tooldomain/ToolSearch.java
+++ src/tooldomain/ToolSearch.java
@@ -37,7 +37,7 @@ public class ToolSearch {
         ResultSet result = null;
 
         //test statement for now to query database
-        result = statement.executeQuery("SELECT * FROM Owner");
+        result = statement.executeQuery("SELECT * FROM \"Owner\"");
         return result;
     }
 
