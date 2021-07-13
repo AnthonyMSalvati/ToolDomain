58020a0a2c68b8b85ff8a43938114e5c289e5f58
AnthonyMSalvati <ams6995@rit.edu>
2021-07-13 14:30:45 -0400

    Search for tool just about working

--- src/tooldomain/ToolSearch.java
+++ src/tooldomain/ToolSearch.java
@@ -1,11 +1,7 @@
 package tooldomain;
 import javax.xml.transform.Result;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 
 public class ToolSearch {
@@ -22,7 +18,7 @@ public class ToolSearch {
                 "true" ).getConnection();
         this.searchType = typeOfSearch;
 
-        processResult(search(""));
+        processResult(search(toolToSearch));
         connection.close();
     }
 
@@ -30,27 +26,22 @@ public class ToolSearch {
         Statement statement = this.connection.createStatement();
         ResultSet result = null;
 
-        //test statement for now to query database
-        result = statement.executeQuery("SELECT * FROM \"Owner\"");
+        String search = String.format("SELECT * FROM \"Tool\" WHERE \"%s\" = \'%s\'", searchType, query);
+        System.out.println(search);
+        result = statement.executeQuery(search);
         return result;
     }
 
     private void processResult(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData meta = resultSet.getMetaData();
         while (resultSet.next())
         {
-            String email = resultSet.getString(1);
-            /*Date createDate = resultSet.getDate(1);
-            String username = resultSet.getString(2);
-            String password = resultSet.getString(3);
-            String firstName = resultSet.getString(4);
-            String lastName = resultSet.getString(5);
-            Date lastAccess = resultSet.getDate(6);*/
-
-            System.out.println("email = " + email);
-            //System.out.println("Create Date = " + createDate.toString());
-            //System.out.println("username = " + username);
-
-
+            for (int i = 0; i < meta.getColumnCount(); i++){
+                if (i > 1) { System.out.print("\t");}
+                String columnVal = resultSet.getString(i);
+                System.out.print(columnVal + " " + meta.getColumnName(i));
+            }
+            System.out.println("");
         }
         resultSet.close();
         connection.close();
