1b35e75279877e77944909aeef9c169823157dad
AnthonyMSalvati <ams6995@rit.edu>
2021-07-13 20:43:48 -0400

    Added chikeud's code that didnt come through for some reason

--- src/tooldomain/Application.java
+++ src/tooldomain/Application.java
@@ -57,8 +57,50 @@ public class Application {
             }
         }
     }
-    public void manageTools(){
-        System.out.println("manage");
+    public void manageTools() throws ClassNotFoundException, SQLException {
+
+        Class.forName("org.postgresql.Driver");
+        connection = new DatabaseConnection(
+                "jdbc:postgresql://reddwarf.cs.rit.edu:5432/p32001a",
+                "Hoh2saikaequeic5piut",
+                "p32001a",
+                "true" ).getConnection();
+
+        System.out.println("------------------------------------------");
+        System.out.println("What would you like to do to your catalog?");
+        System.out.println("------------------------------------------");
+        System.out.println("1. Add Tool \t 2. Delete Tool");
+        System.out.println("3. Edit Tool \t 4. Quit");
+
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+        String input = scanner.nextLine();
+        String order ="";
+
+        switch (Integer.parseInt(input)){
+            case (1) -> {
+                System.out.println("Please enter your email address");
+                String email = scanner.nextLine();
+                System.out.println("Please enter your the barcode number for the tool to add");
+                int value = scanner.nextInt();
+                String query = "INSERT INTO Owner (Email, Barcode)" +
+                        "VALUES " +  email+ "," + value + ");";
+                connection.createStatement().executeQuery(query);
+            }
+            case (2) -> {
+                System.out.println("Please enter your email address");
+                String email = scanner.nextLine();
+                System.out.println("Please enter your the barcode number for the tool to delete");
+                int value = scanner.nextInt();
+                String query = "DELETE FROM Owners" +
+                        "WHERE email = "+ email +
+                        "AND barcode = " + value + ";";
+                connection.createStatement().executeQuery(query);
+            }
+            case (3) -> {
+
+            }
+        }
+
     }
 
     public void searchForTools() throws SQLException, ClassNotFoundException {
